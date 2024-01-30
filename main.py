4e119eb536304578214057414344ea591e537282
Utkarsha3406 <up3406@srmist.edu.in>
2024-01-30 23:11:06 +0530

    extract data from weather api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 st.title("weather forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
@@ -9,11 +9,7 @@ option = st.selectbox("Select data to view",("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = [11,12,13]
-    temperatures = [10,11,15]
-    temperatures = [days * i for i in temperatures]
-    return dates,temperatures
+data = get_data(place, days, option)
 
 d,t = get_data(days)
 figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature (C)"})
